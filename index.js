b1ea120ba7eba558d039ffb7d0d267fdfd762097
Mahesh-addagatla <maheshaddagatla1234@gmail.com>
2024-03-17 11:58:10 +0530

    Remove sensitive infowrom key.json

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.get('/signup', (req, res) => {
 })
 
 const axios = require('axios');
-app.use(express.static('public'));
+
+app.use(express.static('assets'));
 
 const bcrypt = require('bcryptjs');
 
@@ -138,98 +139,4 @@ app.post('/signinsubmit',(req,res)=>{
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var express= require("express");
-// var request=require("request");
-// var app = express()
-
-// app.use(express.static('public'));
-
-// app.set("view engine","ejs");
-
-// app.get('/', function (req, res) {
-//     res.sendFile(__dirname+"/public/js/"+"app.html");
-// })
-// app.listen(5000, function () {
-//     console.log('Example app listening on port 3000!');
-// })
-// app.get("/signupsubmit", function(req,res){
-//     console.log("you are logged in");
-//     console.log(req.query);
-// })
-// app.get("/getmovie",function(req,res){
-//     res.sendFile(__dirname+"/movie.html");
-// })
-// app.get("/moviename",function(req,res){
-//     const mname=req.query.mname;
-//     console.log(req.query);
-//     request(
-//         "http://www.omdbapi.com/?t="+mname+"&apikey=4b60d3b3",
-//         function(error,response,body){
-//             if(JSON.parse(body).Response=="True"){
-//                 console.log(JSON.parse(body));
-//                 //res.send({title:JSON.parse(body).Title,year:JSON.parse(body).Year});
-//                 res.render("moviesinfo");
-//             }
-//             else{
-//                 res.send("something is wrong");
-//             }
-//         }
-//     );
-// });
+});
